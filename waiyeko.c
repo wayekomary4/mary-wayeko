d66a7c6e4214fefce7b266840b417bcc9eaf50af
wayekomary4 <91968793+wayekomary4@users.noreply.github.com>
2021-10-08 16:19:44 +0300

    Add files via upload

---
+++ waiyeko.c
@@ -0,0 +1,12 @@
+/*Arithmetic operators division */
+int main ()
+{
+ int x,y,division;
+ printf("\n enter value to x:");
+ scanf("%d",&x);
+ printf("\n enter value to y:");
+ scanf("%d",&y);
+ division=x/y;
+ printf("\n division=%d",division);
+ return 0;
+}
